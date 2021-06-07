db5372b5af3055193e1ecda357d780abe8751a0b
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-06-07 15:10:52 -0700

    Adds restful routes and actions and validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,4 @@
 class Place < ApplicationRecord
+  validates :name, :address, presence: true
+  validates :address, :uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: "San Diego Zoo", address: "2920 Zoo Dr. San Diego, CA")
+Place.create(name: "Battle Mage Brewery", address: "2870 Scott St. 102, Vista, CA")
+Place.create(name: "Teri Cafe", address: "3809 Plaza Dr. 101, Oceanside, CA")
